08c9a15974fb9a2463395328754728fc426be48d
Francis Pelletier <francispelletier2@hotmail.com>
2022-10-23 19:30:10 -0400

    Add play sound on click on the div element and keyDown for keyboard shortcut

--- src/components/drumPad.tsx
+++ src/components/drumPad.tsx
@@ -1,3 +1,5 @@
+import { useRef, useEffect } from "react";
+
 export interface drumPadParam {
   text: string;
   id: string;
@@ -6,10 +8,27 @@ export interface drumPadParam {
 
 export default function drumPad(props: drumPadParam): JSX.Element {
   const { text, id, source } = props;
+  const audioEl = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      if (!audioEl.current) throw Error("audioEl is not assigned");
+      if (e.key === audioEl.current.id) {
+        audioEl.current.play();
+      }
+    });
+  });
+
+  function handlePadClick() {
+    if (!audioEl.current) throw Error("audioEl is not assigned");
+
+    audioEl.current.play();
+  }
+
   return (
-    <div className="drum-pad" id={id}>
+    <div className="drum-pad" id={id} onClick={handlePadClick}>
       {text}
-      <audio src={source} className="clip" id={text}></audio>
+      <audio ref={audioEl} src={source} className="clip" id={text}></audio>
     </div>
   );
 }
